cb4f1d3e247f3fd0b4c0fd49cbf0318a79520e1a
krakjamone@gmail.com <>
2024-01-27 16:17:41 +0100

    SoundManager

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using Sound;
+using UnityEngine;
+
+public class SoundManager : SerializedMonoBehaviour
+{
+    [SerializeField] private Dictionary<SoundType, AudioClip[]> sounds;
+    [SerializeField] private AudioSource sourcePrefab;
+
+    private List<AudioSource> sourcePool;
+    private List<AudioSource> sourceInUse;
+    private List<AudioSource> swapper;
+
+    public void Play(SoundType soundType, Vector3 position)
+    {
+        var collection = sounds[soundType];
+        var sound = collection[Random.Range(0, collection.Length)];
+
+        var source = GetSource();
+        source.transform.position = position;
+        source.PlayOneShot(sound);
+    }
+
+    private AudioSource GetSource()
+    {
+        if (sourcePool.Count == 0)
+        {
+            var newSource = Instantiate(sourcePrefab, transform);
+            sourceInUse.Add(newSource);
+            return newSource;
+        }
+
+        var source = sourcePool.First();
+        sourcePool.Remove(source);
+        sourceInUse.Add(source);
+        return source;
+    }
+
+    private void Update()
+    {
+        foreach (var source in sourceInUse)
+        {
+            if (!source.isPlaying)
+                swapper.Add(source);
+        }
+
+        foreach (var source in swapper)
+        {
+            sourceInUse.Remove(source);
+            sourcePool.Add(source);
+        }
+        
+        swapper.Clear();
+    }
+}
